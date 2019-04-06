11385d34d2d53da4d638d3a4f75c5c69eb4341b7
skleju <49347278+skleju@users.noreply.github.com>
2019-04-06 19:23:20 +0200

    Main

---
+++ src/main/java/net/lowmc/core/CorePlugin.java
@@ -0,0 +1,13 @@
+package net.lowmc.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CorePlugin extends JavaPlugin{
+	
+	public void onEnable() {
+		getLogger().info("------( LOWMCCORE )-------");
+		getLogger().info("-> Licencja: Sprawdzona");
+		getLogger().info("-> Autorzy: [Piechuuu,skleju]");
+	}
+
+}
